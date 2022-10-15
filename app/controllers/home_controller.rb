a16b98d799b76e35d3baf2b38fca871e115cad44
YunusErdemSihhatli <yunus.erdem@vngrs.com>
2022-10-15 14:28:14 +0300

    Fix paginatable_array variable nil error.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    # @paginatable_array = Kaminari.paginate_array([], total_count: 0).page(params[:page])
     if params[:name].present?
       res_body = Github::Searh.new(name: params[:name], options: { page: params[:page]} ).call
       @items = res_body['items'] if res_body['items'].present?
